a729d16f4e7affae18c1e8d67ac8802cad2da2aa
August Bright <augustbright@yandex.ru>
2023-06-12 01:18:51 +0400

    modified:   .gitignore 	modified:   index.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   tsconfig.json

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { config } from 'dotenv';
 import { OpenAIApi, Configuration } from 'openai';
-
+import admin from 'firebase-admin';
 config();
 
 if (!process.env.TELEGRAM_BOT_TOKEN) {
@@ -11,6 +11,25 @@ if (!process.env.TELEGRAM_BOT_TOKEN) {
 if (!process.env.OPENAI_API_KEY) {
     throw new Error('OPENAI_API_KEY is not defined');
 }
+
+
+admin.initializeApp({    
+    credential: admin.credential.cert({
+        "type": process.env.FIRE_TYPE,
+        "project_id": process.env.FIRE_PROJECT_ID,
+        "private_key_id": process.env.FIRE_PRIVATE_KEY_ID,
+        "private_key": process.env.FIRE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        "client_email": process.env.FIRE_CLIENT_EMAIL,
+        "client_id": process.env.FIRE_CLIENT_ID,
+        "auth_uri": process.env.FIRE_AUTH_URI,
+        "token_uri": process.env.FIRE_TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.FIRE_AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.FIRE_CLIENT_X509_CERT_URL,
+        "universe_domain": process.env.FIRE_UNIVERSE_DOMAIN,
+    } as any)
+});
+
+const db = admin.firestore();
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -27,9 +46,12 @@ type TMessage = {
     party: 'User' | 'Bot'
 };
 
-const chatsHistory: Record<number, TMessage[]> = {};
-
-const createPropmt = (chatHistory: TMessage[]) => {
+const createPropmt = async (chatId: string) => {
+    const snapshot = await db.collection('chats').doc(chatId).collection('messages').orderBy("timestamp").get();
+    const chatHistory = snapshot.docs.map((doc) => {
+        const data = doc.data() as TMessage;
+        return data;
+    });
     const zeroPrompt = `
         Перед тобой чат, в готором Человек общается с тобой.
         Ты - Бог.
@@ -38,28 +60,30 @@ const createPropmt = (chatHistory: TMessage[]) => {
 
     `;
 
-    const textLog = chatHistory.map((message) => {        
-        return `${message.party}: ${message.message}`;
+    console.log('chatHistory', chatHistory);
+
+    const textLog = chatHistory.map((message) => {
+        return `${message.party === 'Bot' ? 'Бог' : 'Человек'}: ${message.message}`;
     });
     return zeroPrompt + textLog.join('\n') + '\nБог:';
 };
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', async (msg) => {    
+bot.on('message', async (msg) => {
+    if (msg.text?.startsWith('/')) { return; }
+
     const chatId = msg.chat.id;
-    if (!chatsHistory[chatId]) {
-        chatsHistory[chatId] = [];
-    }
-    const chatHistory = chatsHistory[chatId];
-    chatHistory.push({
-        message: msg.text || '',
-        party: 'User'
+    bot.sendChatAction(chatId, 'typing');
+
+    await db.collection('chats').doc(chatId.toString()).collection('messages').add({
+        message: msg.text,
+        party: 'User',
+        timestamp: admin.firestore.FieldValue.serverTimestamp()
     });
 
-    const prompt = createPropmt(chatHistory);
+    const prompt = await createPropmt(chatId.toString());
     console.log('prompt', prompt);
-    bot.sendChatAction(chatId, 'typing');
 
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
@@ -72,9 +96,11 @@ bot.on('message', async (msg) => {
     });
 
     const answer = completion.data.choices[0].text || '';
-    chatHistory.push({
+
+    await db.collection('chats').doc(chatId.toString()).collection('messages').add({
         message: answer,
-        party: 'Bot'
+        party: 'Bot',
+        timestamp: admin.firestore.FieldValue.serverTimestamp()
     });
 
     bot.sendMessage(chatId, answer);
